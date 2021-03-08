5e71b4333f9b9d16bd25a3887c60e0d59905e43e
luqing-92 <457172728@qq.com>
2021-03-08 11:35:23 +0800

    1_test_add_push.c

---
+++ 1_test_add_push.c
@@ -0,0 +1,22 @@
+/*================================================================
+*   Copyright (C) 2021 hqyj Ltd. All rights reserved.
+*   
+*   文件名称：1_test_add_push.c
+*   创 建 者：luqing
+*   创建日期：2021年03月08日
+*   描    述：
+*
+================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	printf("luqing\n");	
+	
+	return 0;
+}
+
+
